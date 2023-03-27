00cf53ed99d7db7208f53ac3846938e552f873b4
lromeraj <lromerajdev@gmail.com>
2023-03-27 22:15:04 +0200

    Initial commit

---
+++ src/isbdemu.ts
@@ -0,0 +1,24 @@
+import { SerialPort } from "serialport"
+import { Argument, Command, Option, program } from "commander";
+
+program
+  .version( '0.0.1' )
+  .description( 'A simple emulator for Iridium SBD 9602/9602 transceivers' )
+
+program.addOption( 
+  new Option( '-p, --path <string>', 'Serial port path' ).makeOptionMandatory() )
+
+async function main() {
+  
+  program.parse();
+  const opts = program.opts();
+
+  const serialport = new SerialPort({ path: opts.path, baudRate: 19200 })
+
+  serialport.write( '\r\nOK\r\n' );
+  serialport.close();
+}
+
+main();
+
+
